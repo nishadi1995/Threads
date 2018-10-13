26dacbd906475593d6c8f10d2beff0f5cd289226
nishadi1995 <nishadinuwa1995@gmail.com>
2018-10-13 21:15:53 +0530

    second commit

--- src/threads/ThreadsB.java
+++ src/threads/ThreadsB.java
@@ -1,38 +1,53 @@
 package threads;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class ThreadsB implements Runnable {
 
-    int[] numbers;
-    public int sum,low,high=0;
+    int myid;
+    static int[] numbers;
+    public static int sum, low, high = 0;
 
-    void creatArray() {
+    ThreadsB(int a) {
+        myid = a;
+    }
+
+    static void creatArray() {
         numbers = new int[1000];
         for (int i = 0; i < 1000; i++) {
             numbers[i] = i + 1;
         }
     }
 
-    public void run() {
-        int wsize=1000/2;
-        low;
-        high= low+wsize;
+    public synchronized void run() {
+        int wsize = 1000 / 2;
+        low = wsize * myid;
+        high = low + wsize;
         for (int j = low; j < high; j++) {
             sum += numbers[j];
         }
-        System.out.println(numbers[2]);
     }
 }
 
 class TestThread {
 
     public static void main(String[] args) {
-        ThreadsB a = new ThreadsB();
-        a.creatArray();
-        Thread t1 = new Thread(a, "Thread 1");
-        Thread t2 = new Thread(a, "Thread 2");
-        t1.start();
-        t2.start();
-        System.out.println("sum " + a.sum);
+        try {
+            ThreadsB a = new ThreadsB(0);
+            ThreadsB b = new ThreadsB(1);
+            ThreadsB.creatArray();
+            
+            Thread t1 = new Thread(a, "Thread 1");
+            Thread t2 = new Thread(b, "Thread 2");
+            t1.start();
+            t1.join();
+            t2.start();
+            t2.join();
+            System.out.println("sum " + a.sum);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(TestThread.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
     }
 }
